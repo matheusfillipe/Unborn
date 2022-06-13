5c3d2a306b0eea17d1c85f310f0cc4e3359b0aeb
loudercake <jxzk@protonmail.com>
2022-06-13 20:50:26 -0300

    icon

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -5,8 +5,8 @@ export var speed = 5
 var target
 
 func _ready():
-    if target_path:
-        target = get_node(target_path)
+	if target_path:
+		target = get_node(target_path)
 
 func _physics_process(delta):
-    position = lerp(position, target.position, speed * delta)
+	position = lerp(position, target.position, speed * delta)
